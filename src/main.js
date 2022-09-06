8b8ccefabcea42cc65c87920a531f8607c1e19be
danliris <danliris.cloud@gmail.com>
2022-09-06 09:23:01 +0700

    Update main.js

--- src/main.js
+++ src/main.js
@@ -30,11 +30,11 @@ export async function configure(aurelia) {
         },
       };
 
-      var auth = "https://com-batiksolo-service-auth-uat.azurewebsites.net/v1/";
-      var master = "https://com-batiksolo-service-core-uat.azurewebsites.net/v1/";
-      var inventory = "https://com-batiksolo-service-warehouse-uat.azurewebsites.net/v1/";
-      var merchandiser = "https://com-batiksolo-service-warehouse-uat.azurewebsites.net/v1/";
-      var sales = "https://com-batiksolo-service-pos-uat.azurewebsites.net/v1/";
+      var auth = "https://com-batiksolo-service-auth.azurewebsites.net/v1/";
+      var master = "https://com-batiksolo-service-core.azurewebsites.net/v1/";
+      var inventory = "https://com-batiksolo-service-warehouse.azurewebsites.net/v1/";
+      var merchandiser = "https://com-batiksolo-service-warehouse.azurewebsites.net/v1/";
+      var sales = "https://com-batiksolo-service-pos.azurewebsites.net/v1/";
       //Config API
       config.registerEndpoint("auth", auth);
       config.registerEndpoint("master", master);
